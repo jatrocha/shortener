eac631fa1f6522b208ca14452f8d7f174109d6c0
Joao Rocha <jatrocha@gmail.com>
2020-03-14 10:49:49 +0000

    New project's structure

---
+++ src/test/java/org/cygnus/web/shortener/ShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cygnus.web.shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
